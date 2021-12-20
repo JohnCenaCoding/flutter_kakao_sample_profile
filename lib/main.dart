dc30fe3dd50fa3721af714a0e393bd353f86ccc5
JohnCenaCoding <wjdwo1735@naver.com>
2021-12-20 22:59:25 +0900

    fourth Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kakao_sample_profile/src/controller/profile_controller.dart';
 import 'package:kakao_sample_profile/src/profile.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(MyApp());
 }
 
@@ -12,6 +25,9 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.light().copyWith(primaryColor: Colors.white),
+      initialBinding: BindingsBuilder(() {
+        Get.lazyPut<ProfileController>(() => ProfileController());
+      }),
       home: Profile(),
     );
   }
--- lib/src/profile.dart
+++ lib/src/profile.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kakao_sample_profile/src/controller/profile_controller.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends GetView<ProfileController> {
   const Profile({Key? key}) : super(key: key);
 
   @override
@@ -13,6 +14,7 @@ class Profile extends StatelessWidget {
           children: [
             _backgroundImage(),
             _header(),
+            _myProfile(),
             _footer(),
           ],
         ),
@@ -82,30 +84,101 @@ class Profile extends StatelessWidget {
     );
   }
 
+  Widget _oneButton(IconData icon, String title, void Function()? ontap) {
+    return GestureDetector(
+      onTap: ontap,
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+          ),
+          SizedBox(height: 10),
+          Text(
+            title,
+            style: TextStyle(fontSize: 12, color: Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+
   Widget _footer() {
     return Positioned(
       bottom: 0,
       left: 0,
       right: 0,
       child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            border: Border(
+                top: BorderSide(
+          width: 1,
+          color: Colors.white.withOpacity(0.4),
+        ))),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Column(
-              children: [
-                Icon(
-                  Icons.chat_bubble,
-                  color: Colors.white,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "나와의 채탕",
-                  style: TextStyle(fontSize: 12, color: Colors.white),
-                )
-              ],
-            )
+            _oneButton(Icons.chat_bubble, "나와의 채팅", () {}),
+            _oneButton(Icons.edit, "프로필 편집", controller.toggleEditProfile),
+            _oneButton(Icons.chat_bubble_outline, "카카오스토리", () {}),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _myProfile() {
+    return Obx(
+      () => Positioned(
+        bottom: 130,
+        left: 0,
+        right: 0,
+        child: Container(
+          height: 200,
+          child: Column(
+            children: [
+              _profileImage(),
+              _profileInfo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImage() {
+    return Container(
+      width: 120,
+      height: 120,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(40),
+        child: Image.network(
+          "https://i.stack.imgur.com/l60Hf.png",
+          //cover는 이미지를 풀로 채워줌.
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _profileInfo() {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Text(
+            "존시나",
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.w400, color: Colors.white),
+          ),
+        ),
+        Text(
+          "FU와 U can't see me ",
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.w400, color: Colors.white),
+        ),
+      ],
     );
   }
 }
